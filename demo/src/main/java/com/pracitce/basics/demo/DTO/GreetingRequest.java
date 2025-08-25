453820d121da64306a9af06a5bc4cebb8c4df8d1
srinivas <srinivas>
2025-08-25 12:42:00 +0530

    Added greet post method

---
+++ demo/src/main/java/com/pracitce/basics/demo/DTO/GreetingRequest.java
@@ -0,0 +1,13 @@
+package com.pracitce.basics.demo.DTO;
+
+public class GreetingRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- demo/src/main/java/com/pracitce/basics/demo/controller/HomeController.java
+++ demo/src/main/java/com/pracitce/basics/demo/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.pracitce.basics.demo.controller;
 
+import com.pracitce.basics.demo.DTO.GreetingRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/")
 public class HomeController {
 
+    final String greetingTemplate="Hello %s how are you?.";
     @GetMapping("hi")
     public String hi(){
         return "helloWorld";
@@ -18,6 +18,13 @@ public class HomeController {
     @GetMapping("hello")
     public ResponseEntity<?> hello(){
         ResponseEntity<String> response= new ResponseEntity<>("Hello World",HttpStatus.OK);
+        return response;
+    }
+
+    @PostMapping("greet")
+    public ResponseEntity<?> greet(@RequestBody GreetingRequest greetingRequest){
+
+        ResponseEntity<String> response= new ResponseEntity<>(greetingTemplate.formatted(greetingRequest.getName()),HttpStatus.OK);
         return response;
     }
 }
